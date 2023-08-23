205d02c7dc6e0873c241fa375153584276bebe50
Mohammad Alasli <mohammedalasly5@gmail.com>
2023-08-23 14:59:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
-  databaseURL: "add your own Firebase project URL here",
+  databaseURL: "https://add-to-card-75e4a-default-rtdb.europe-west1.firebasedatabase.app/",
 };
 
 const app = initializeApp(appSettings);
